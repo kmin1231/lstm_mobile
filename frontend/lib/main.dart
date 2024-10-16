4b375281c58a71af9a35b31f8f02c3e20cd7e27b
kmin1231 <kmin91143@gmail.com>
2024-10-16 16:17:44 +0900

    WIP: finnhub API

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
+
 import 'responsive_layout.dart';
 import 'home_screen.dart';
+
 import 'dart:async';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 void main() async {
+  await dotenv.load(fileName: ".env");
   // WidgetsFlutterBinding.ensureInitialized();
   // await Firebase.initializeApp();
   runApp(const MyApp());
