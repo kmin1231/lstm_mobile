d658a3384af9cb034cdd1f858748b319af517060
kmin1231 <kmin91143@gmail.com>
2024-12-08 18:00:08 +0900

    file for statistical analysis updated: margin ratio

--- algorithm/stats.py
+++ algorithm/stats.py
@@ -2,7 +2,7 @@ import sqlite3
 import pandas as pd
 import os
 
-margin = 5000
+# margin = 5000
 
 
 def read_db(db_file, table_name):
@@ -17,7 +17,7 @@ def read_db(db_file, table_name):
     return df
 
 
-def stats(df):
+def stats(df, margin_ratio):
 
     # <1> basic info
     first_date = df['Date'].min()
@@ -31,19 +31,20 @@ def stats(df):
     avg_difference = df['Difference'].abs().mean()
 
     # <3> error range
+    margin = avg_actual * margin_ratio
     count_within_range = (df['Difference'].abs() <= margin).sum()
 
     # <4> prediction accuracy
     accuracy = (count_within_range / count) * 100
 
-    return first_date, last_date, count, avg_actual, avg_prediction, avg_difference, count_within_range, accuracy
+    return first_date, last_date, count, avg_actual, avg_prediction, avg_difference, margin, count_within_range, accuracy
 
 
-def print_stats(df, db_file):
+def print_stats(df, db_file, table_name, margin_ratio):
     df = read_db(db_file, table_name)
     file_name = os.path.basename(db_file)
         
-    first_date, last_date, count, avg_actual, avg_prediction, avg_difference, count_within_range, accuracy = stats(df)
+    first_date, last_date, count, avg_actual, avg_prediction, avg_difference, margin, count_within_range, accuracy = stats(df, margin_ratio)
 
     print(f"\nFile Name: [{file_name}]")
     print("\n>> Basic Info")
@@ -63,10 +64,10 @@ def print_stats(df, db_file):
     print(f"* number of data within range: {count_within_range}")
     print(f"* accuracy: {accuracy:.2f}%")
 
-    return accuracy
+    return accuracy, margin
 
 
-def print_accuracy(db_files, table_name):
+def print_accuracy(db_files, table_name, margin_ratio):
     print("005930 삼성전자      000660 SK하이닉스      373220 LG에너지솔루션")
     print("207940 삼성바이오로직스      005380 현대차      005935 삼성전자우")
     print("068270 셀트리온   000270 기아   105560 KB금융   005490 POSCO홀딩스\n")
@@ -74,27 +75,27 @@ def print_accuracy(db_files, table_name):
     for db_file in db_files:
         file_name = os.path.basename(db_file)
         df = read_db(db_file, table_name)
-        values = stats(df)
+        _, _, _, avg_actual, _, _, margin, _, accuracy = stats(df, margin_ratio)
         
-        if values: _, _, _, _, _, _, _, accuracy = values
-        print(f"{file_name}: {accuracy:.2f}%")
+        print(f"{file_name}: {accuracy:.2f}% (margin: {margin:,.0f})")
+        # print(f"{accuracy:.2f}% ({margin:,.0f})")
 
 
-def multiple_db_files(db_files, table_name):
+
+def multiple_db_files(db_files, table_name, margin_ratio):
     for db_file in db_files:
         df = read_db(db_file, table_name)
-        stats_values = stats(df)
-        print_stats(df, db_file)    
+        # stats_values = stats(df)
+        accuracy, marin = print_stats(df, db_file, table_name, margin_ratio)    
         print()
         print()
 
 
 db_files = [
-              # files
+
 ]
 
-
-# table_name = 'lstm_prediction'
+margin_ratio = 0.02
 table_name = 'predictions'
 
 # for each file
@@ -103,7 +104,7 @@ table_name = 'predictions'
 # print_stats(df, db)
 
 # multiple files
-# multiple_db_files(db_files, table_name)
+# multiple_db_files(db_files, table_name, margin_ratio)
 
 # accuracy
-print_accuracy(db_files, table_name)
+print_accuracy(db_files, table_name, margin_ratio)
